693ce7785f7ef49baa45f952751b77fee18f57dc
Andrea <b.a.benvenuti3@gmail.com>
2021-03-10 16:20:44 +0100

    Aggiunte piccole migliorie

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 //--------------------- LE FUNZIONI-----------------------------
-// genera numero random tra due estremi compresi
+// restituisce un numero random tra due estremi compresi
 function random(min, max){
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  var numero = Math.floor(Math.random() * (max - min + 1)) + min;
+  return numero;
 }
-//restituisce "true" se l'elemento è presente nell'array (la invoco una volta poi uso includes)
-function isInArray(array, element) {
+/*
+restituisce "true" se l'elemento è presente nell'array
+(si può fare con includes al posto di usare questa funzione)
+*/
+function inArray(array, elemento) {
   var i = 0;
   while(i < array.length){
-    if(array[i] == element){
+    if(array[i] == elemento){
       return true;
     }
     i++;
@@ -15,57 +19,76 @@ function isInArray(array, element) {
   return false;
 }
 //--------------------fine DELLE FUNZIONI------------------------
-//-----------------VARIABILI-----------------
-var livello;
+// SELEZIONE DEL LIVELLO DI DIFFICOLTA'
+do {
+  var livello = parseInt(prompt("inserisci il livello di difficolta: 0,1,2"));
+} while (livello !=0 && livello !=1 && livello !=2);
+var parametroDif;
+switch (livello) {
+  case 0:
+    parametroDif = 100;
+    break;
+  case 1:
+    parametroDif = 80;
+    break;
+  case 2:
+    parametroDif = 50;
+    break;
+}
+
+// GENERO L'ARRAY CONTENENTE I NUMERI "BOMBA" (univoci)
 var arrayBombe = [];
-var numeroRandom = 0;
-var doppione = false;
-var arrayGiocatore = [];
-var numeroBombe = 16;
-var numeroUtente;
-//------------fine DELLE VARIABILI-----------------
-
-// imposto livello di dificoltà
-while (livello != 0 && livello != 1 && livello != 2){
-  var livello = parseInt(prompt("Scegli il livello di difficoltà tra 0 e 2"));
-}
-if (livello == 0){
-  max = 100;
-} else if (livello == 1){
-  max = 80;
-} else if (livello == 2) {
-  max = 50;
-}
-
-// creo l'array contenente i numeri "bomba" CHE NON POSSONO RIPETERSI
-for (var i = 0; arrayBombe.length < 16; i++) {
-  numeroRandom = random(1, 100)
-  doppione = isInArray(arrayBombe, numeroRandom)
-  if (doppione == false) {
-    arrayBombe.push(numeroRandom)
+var numBombe = 16;
+while (arrayBombe.length < numBombe) {
+  var numeroRandom = random(1, parametroDif);
+  if (inArray(arrayBombe, numeroRandom) == false) {
+    arrayBombe.push(numeroRandom);
   }
 }
-console.log("I numeri bomba sono: " + arrayBombe);
 
 // CORPO DEL PROGRAMMA
-while ( arrayGiocatore.length < (max - numeroBombe) && arrayBombe.includes(numeroUtente) == false ){
-    numeroUtente = parseInt(prompt("inserisci un numero"));
+var arrayUtente = [];
+var bombaPresa = false;
+var casiFavorevoli = parametroDif - numBombe;
+// entriamo nel while se le 2 condizioni sono vere (AND)
+while (arrayUtente.length < casiFavorevoli && bombaPresa == false) {
+    var numeroUtente = parseInt(prompt("inserisci un numero"));
     /*
-    validazione del numero inserito:
-    deve essere =  un numero | compreso tra 1 e 100 | non posso inserire sempre lo stesso valore
+    validazioni:
+    - deve esssere un numero e compreso tra 1 e 100 compresi
+    - non posso inserire sempre lo stesso numero
     */
-    if (isNaN(numeroUtente))  {
-      alert("devi inserire solo numeri");
-    } else if (numeroUtente <= 0 || numeroUtente > 100 ){
-      alert("deve essere compreso da 1 e 100");
-    } else if ( arrayGiocatore.includes(numeroUtente) == false ){
-      arrayGiocatore.push(numeroUtente);
+    if (isNaN(numeroUtente) || numeroUtente < 1 || numeroUtente > parametroDif) {
+      alert("ATTENZIONE! devi inserire un numero tra 1 e " + parametroDif);
+    } else if (inArray(arrayUtente, numeroUtente) == true) {
+      alert("Non fare il furbetto");
+    } else if (inArray(arrayBombe, numeroUtente) == true) {
+      bombaPresa = true;
+    }  else {
+      arrayUtente.push(numeroUtente);
     }
 }
 
 // ESITO DELLA PARTITA
-if ( arrayGiocatore.length == (max - numeroBombe)){
-  alert ("CONGRATULAZIONI Hai Vinto!");
-} else{
-  alert("Hai perso il tuo punteggio è: " + (arrayGiocatore.length - 1) );
+var variabileStampEsito = document.getElementById('stamp_esito_title');
+var variabileStampBombe = document.getElementById('stamp_bombe');
+var variabileStampUtente = document.getElementById('stamp_utente');
+var variabileStampPunteggio = document.getElementById('stamp_punteggio');
+
+if (bombaPresa == true) {
+  alert("Hai perso, il tuo punteggio è: " + arrayUtente.length);
+  variabileStampEsito.innerHTML = "Dati della partita:";
+  variabileStampBombe.innerHTML = "I numeri bomba sono: " + arrayBombe;
+  if (arrayUtente?.length > 0) {
+    variabileStampUtente.innerHTML = "I numeri corretti da te inseriti, sono: " + arrayUtente;
+  } else {
+    variabileStampUtente.innerHTML = "CHE SFORTUNA! Non hai inserito nessun numero corretto";
+  }
+  variabileStampPunteggio.innerHTML = "Il tuo punteggio è: " + arrayUtente.length;
+} else {
+  alert("COMPLIMENTI! Hai Vinto.");
+  variabileStampEsito.innerHTML = "Dati della partita:";
+  variabileStampBombe.innerHTML = "I numeri bomba sono: " + arrayBombe;
+  variabileStampUtente.innerHTML = "I numeri corretti da te inseriti, sono: " + arrayUtente;
+  variabileStampPunteggio.innerHTML = "VITTORIA!";
 }
